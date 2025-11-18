f6549cdfcd421ec4392105ab64b9e1ec683f7f8b
DaviNegreiros <dv.neg1711@gmail.com>
2025-11-18 11:34:19 -0300

    ready to merge

--- app/frontend/treina_ai/lib/main.dart
+++ app/frontend/treina_ai/lib/main.dart
@@ -3,9 +3,7 @@ import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'dart:io' show Platform;
 import 'ui/register_page.dart';
 import 'ui/student_search_screen.dart';
-import 'ui/periodo_page.dart'; 
 import 'data/users_database.dart';
-import 'ui/training_register_page.dart';
 
 
 Future<void> main() async {
@@ -47,41 +45,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'TreinaAí',
-       home: TrainingPage(date: "24/11/2024"), // vai pra register_page se não registrado else vai pra student_search_screen
+       home: isUserRegistered ? StudentSearchScreen() : RegisterPage(), // vai pra register_page se não registrado else vai pra student_search_screen
     );
   }
 }
 
-class TestPages extends StatelessWidget {
-  final bool isUserRegistered;
-
-  const TestPages({super.key, required this.isUserRegistered});
-
-  @override
-  Widget build(BuildContext context) {
-    // mapa das páginas que quer testar
-    final pages = {
-      'Register Page': RegisterPage(),
-      'Student Search': StudentSearchScreen(),
-      'Periodo Page': PeriodoPage(),
-      // adicione outras páginas aqui
-    };
-
-    return Scaffold(
-      appBar: AppBar(title: const Text('Test Pages')),
-      body: ListView(
-        children: pages.entries.map((entry) {
-          return ListTile(
-            title: Text(entry.key),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => entry.value),
-              );
-            },
-          );
-        }).toList(),
-      ),
-    );
-  }
-}
